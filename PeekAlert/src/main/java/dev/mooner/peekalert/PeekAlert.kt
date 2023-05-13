c46f189514fbb193b53ed18abf7b6d8ff2954f9a
mooner1022 <moonmk10221@gmail.com>
2023-05-13 17:46:31 +0900

    Add javadoc comments

--- PeekAlert/src/main/java/dev/mooner/peekalert/PeekAlert.kt
+++ PeekAlert/src/main/java/dev/mooner/peekalert/PeekAlert.kt
@@ -1,3 +1,11 @@
+/**
+ * PeekAlert created by Minki Moon(mooner1022)
+ * Copyright (c) mooner1022. all rights reserved.
+ * This code is licensed under the MIT License.
+ *
+ * @author mooner1022
+ */
+
 package dev.mooner.peekalert
 
 import android.app.Activity
@@ -44,88 +52,163 @@ class PeekAlert internal constructor(
     private var autoHideMillis: Long? = null
     private val actionConfig  : ActionConfig by lazy { ActionConfig() }
 
+    /**
+     * Set the position of alert to be shown
+     * @param position : Position of alert
+     * @see Position
+     */
     fun setPosition(position: Position): PeekAlert {
         this.position = position
         return this
     }
 
+    /**
+     * Set the background color of alert
+     * @param res : Color resource
+     * @param color : Color int
+     */
     @JvmOverloads
     fun setBackgroundColor(@ColorRes res: Int? = null, @ColorInt color: Int? = null): PeekAlert {
         this.bgColor = res?.let(parentView.context::getColor) ?: color
         return this
     }
 
+    /**
+     * Set corner radius of alert
+     * @param radius : radius of corner, by px
+     */
     fun setCornerRadius(@Px radius: Float): PeekAlert {
         this.radius = radius
         return this
     }
 
+    /**
+     * Set width of alert view
+     * Should be set as one of MATCH_PARENT or WRAP_CONTENT, unless you have a particular purpose
+     * @param width : width of view
+     */
     fun setWidth(width: Int): PeekAlert {
         this.width = width
         return this
     }
 
+    /**
+     * Set title of alert
+     * @param title : The desired content of title text
+     */
     fun setTitle(title: String): PeekAlert {
         titleConfig.content = title
         return this
     }
 
+    /**
+     * Set size of title text
+     * @param size : The scaled pixel size
+     */
     fun setTitleSize(size: Float): PeekAlert {
         titleConfig.size = size
         return this
     }
 
+    /**
+     * Set color of title text
+     * @param res : Resource value of color
+     * @param color : Int value of color
+     */
     @JvmOverloads
     fun setTitleColor(@ColorRes res: Int? = null, @ColorInt color: Int? = null): PeekAlert {
         titleConfig.color = res?.let(parentView.context::getColor) ?: color
         return this
     }
 
+    /**
+     * Set typeface of title text
+     * @param typeface : The desired typeface of title
+     */
     fun setTitleTypeface(typeface: Typeface): PeekAlert {
         titleConfig.typeface = typeface
         return this
     }
 
+    /**
+     * Set text of alert
+     * @param text : The desired content of text
+     */
     fun setText(text: String): PeekAlert {
         textConfig.content = text
         return this
     }
 
+    /**
+     * Set size of text
+     * @param size : The scaled pixel size
+     */
     fun setTextSize(size: Float): PeekAlert {
         textConfig.size = size
         return this
     }
 
+    /**
+     * Set color of text
+     * @param res : Resource value of color
+     * @param color : Int value of color
+     */
     @JvmOverloads
     fun setTextColor(@ColorRes res: Int? = null, @ColorInt color: Int? = null): PeekAlert {
         textConfig.color = res?.let(parentView.context::getColor) ?: color
         return this
     }
 
+    /**
+     * Set typeface of text
+     * @param typeface : The desired typeface of text
+     */
     fun setTextTypeface(typeface: Typeface): PeekAlert {
         textConfig.typeface = typeface
         return this
     }
 
+    /**
+     * Set icon of alert
+     * @param res : Drawable resource of desired icon content
+     */
     fun setIcon(@DrawableRes res: Int?): PeekAlert {
         this.iconRes = res
         return this
     }
 
+    /**
+     * Set tint color of icon
+     * @param res : Resource value of color
+     * @param color : Int value of color
+     */
     @JvmOverloads
     fun setIconTint(@ColorRes res: Int? = null, @ColorInt color: Int? = null): PeekAlert {
         this.iconTint = res?.let(parentView.context::getColor) ?: color
         return this
     }
 
+    /**
+     * Set padding of alert, in px
+     * @param px : The desired padding in px
+     */
     fun setPaddingPx(@Px px: Int): PeekAlert {
         this.paddingPx = px
         return this
     }
 
+    /**
+     * Set padding of alert, in dp
+     * @param dp : The desired padding in dp
+     */
     fun setPaddingDp(dp: Int): PeekAlert =
         setPaddingPx(dp.dp)
 
+    /**
+     * Set horizontal, vertical margin of alert
+     * @param horizontal : The desired horizontal margin of alert, in px
+     * @param vertical : The desired vertical margin of alert, in px
+     */
     fun setMargin(
         @Dimension(unit = Dimension.DP) horizontal: Int? = null,
         @Dimension(unit = Dimension.DP) vertical: Int? = null
@@ -134,16 +217,26 @@ class PeekAlert internal constructor(
         return this
     }
 
+    /**
+     * Set alert to be dragged and dismissed by user
+     */
     fun setDraggable(draggable: Boolean): PeekAlert {
         this.draggable = draggable
         return this
     }
 
+    /**
+     * Set alert to be hidden on user touch
+     */
     fun setHideOnTouch(hide: Boolean): PeekAlert {
         this.hideOnTouch = hide
         return this
     }
 
+    /**
+     * Set alert to be automatically hidden after 3000ms
+     * @see setAutoHide(Long)
+     */
     fun setAutoHide(hide: Boolean): PeekAlert {
         this.autoHideMillis =
             if (hide)
@@ -153,11 +246,21 @@ class PeekAlert internal constructor(
         return this
     }
 
+    /**
+     * Set alert to be automatically hidden after the given delay
+     * @param delay : The desired delay in milliseconds
+     * @see setAutoHide(Boolean)
+     */
     fun setAutoHide(delay: Long = 3000L): PeekAlert {
         this.autoHideMillis = delay
         return this
     }
 
+    /**
+     * Set a user-clickable button shown at right-end of alert
+     * @param config : The desired action config
+     * @see setAction
+     */
     fun setAction(config: ActionConfig): PeekAlert {
         actionConfig.text             = config.text
         actionConfig.backgroundColor  = config.backgroundColor
@@ -166,6 +269,16 @@ class PeekAlert internal constructor(
         return this
     }
 
+    /**
+     * Set a user-clickable button shown at right-end of alert
+     * @param text : The desired text of action button
+     * @param backgroundColorRes : The desired background color of action, as Resource value
+     * @param backgroundColorInt : The desired background color of action, as Color Int value
+     * @param textColorRes : The desired text color of action, as Resource value
+     * @param textColorInt : The desired text color of action, as Color Int value
+     * @param onClick : Callback listener called on user click action
+     * @see setAction
+     */
     @JvmOverloads
     fun setAction(
         text: String?,
@@ -193,6 +306,10 @@ class PeekAlert internal constructor(
         return this
     }
 
+    /**
+     * Show the alert on screen, with settings set before
+     * @see hide
+     */
     fun peek() {
         removeViewFromParent()
 
@@ -273,6 +390,10 @@ class PeekAlert internal constructor(
         }
     }
 
+    /**
+     * Hide the alert currently being shown on the screen if present
+     * @see peek
+     */
     fun hide() {
         if (instance != null)
             hide(instance!!)
